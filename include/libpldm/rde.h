4058f45d0e3171602c7ebd275e8280a9728621fd
Jayanth Othayoth <Jayanth.Othayoth@amd.com>
2025-04-16 07:56:52 -0500

    rde: Add NegotiateRedfishParameters support
    Added an API for encoding/decoding NegotiateRedfishParameters requests
    and responses as per DSP0218 v1.2.0 section 11.1
    
    Change-Id: I6d7e485fa52b4a5a3268e479814f71c41ebbd980
    Signed-off-by: Jayanth Othayoth <Jayanth.Othayoth@amd.com>

--- include/libpldm/rde.h
+++ include/libpldm/rde.h
@@ -1,9 +1,14 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
 #ifndef LIBPLDM_RDE_H
 #define LIBPLDM_RDE_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <libpldm/base.h>
+#include <stddef.h>
+#include <stdint.h>
 
 /** @brief PLDM for Redfish Device Completion Code */
 enum pldm_rde_completion_codes {
@@ -49,8 +54,106 @@ enum pldm_rde_commands {
 	PLDM_RDE_CMD_RDE_MULTIPART_RECEIVE = 0x31,
 };
 
+/* Compatibility aliases used by AMD encode/decode implementations */
+#define PLDM_NEGOTIATE_REDFISH_PARAMETERS \
+	PLDM_RDE_CMD_NEGOTIATE_REDFISH_PARAMETERS
+
+/* Response lengths are inclusive of completion code */
+#define PLDM_RDE_NEGOTIATE_REDFISH_PARAMETERS_REQ_BYTES	    3
+#define PLDM_RDE_NEGOTIATE_REDFISH_PARAMETERS_RESP_MIN_SIZE 12
+
+enum pldm_rde_varstring_format {
+	PLDM_RDE_VARSTRING_UNKNOWN = 0,
+	PLDM_RDE_VARSTRING_ASCII = 1,
+	PLDM_RDE_VARSTRING_UTF_8 = 2,
+	PLDM_RDE_VARSTRING_UTF_16 = 3,
+	PLDM_RDE_VARSTRING_UTF_16LE = 4,
+	PLDM_RDE_VARSTRING_UTF_16BE = 5,
+};
+
+/**
+ * @brief MC feature support.
+ *
+ * The flags can be OR'd together to build the feature support for a MC.
+ */
+enum pldm_rde_mc_feature {
+	PLDM_RDE_MC_FEATURE_HEAD_SUPPORTED = 1 << 0,
+	PLDM_RDE_MC_FEATURE_READ_SUPPORTED = 1 << 1,
+	PLDM_RDE_MC_FEATURE_CREATE_SUPPORTED = 1 << 2,
+	PLDM_RDE_MC_FEATURE_DELETE_SUPPORTED = 1 << 3,
+	PLDM_RDE_MC_FEATURE_UPDATE_SUPPORTED = 1 << 4,
+	PLDM_RDE_MC_FEATURE_REPLACE_SUPPORTED = 1 << 5,
+	PLDM_RDE_MC_FEATURE_ACTION_SUPPORTED = 1 << 6,
+	PLDM_RDE_MC_FEATURE_EVENTS_SUPPORTED = 1 << 7,
+	PLDM_RDE_MC_FEATURE_BEJ_1_1_SUPPORTED = 1 << 8,
+	// Reserved bits [15:9]
+};
+
+/**
+ * @brief Device feature support.
+ *
+ * The flags can be OR'd together to build the feature support for an RDE Device.
+ */
+enum pldm_rde_device_feature_support {
+	PLDM_RDE_DEVICE_FEATURE_HEAD_SUPPORTED = 1 << 0,
+	PLDM_RDE_DEVICE_FEATURE_READ_SUPPORTED = 1 << 1,
+	PLDM_RDE_DEVICE_FEATURE_CREATE_SUPPORTED = 1 << 2,
+	PLDM_RDE_DEVICE_FEATURE_DELETE_SUPPORTED = 1 << 3,
+	PLDM_RDE_DEVICE_FEATURE_UPDATE_SUPPORTED = 1 << 4,
+	PLDM_RDE_DEVICE_FEATURE_REPLACE_SUPPORTED = 1 << 5,
+	PLDM_RDE_DEVICE_FEATURE_ACTION_SUPPORTED = 1 << 6,
+	PLDM_RDE_DEVICE_FEATURE_EVENTS_SUPPORTED = 1 << 7,
+	// Reserved bits [15:8]
+};
+
+/**
+ * @brief Device capability flags.
+ *
+ * The flags can be OR'd together to build capabilities of a device.
+ */
+enum device_capabilities_flags {
+	PLDM_RDE_DEVICE_CAP_ATOMIC_RESOURCE_READ = 1 << 0,
+	PLDM_RDE_DEVICE_CAP_EXPAND_SUPPORT = 1 << 1,
+	PLDM_RDE_DEVICE_CAP_BEJ_1_1_SUPPORT = 1 << 2,
+	// Reserved bits [7:3]
+};
+
+/* @brief varstring PLDM data type */
+struct pldm_rde_varstring {
+	uint8_t string_format;	     // Format of the string
+	uint8_t string_length_bytes; // Length of the string including NULL terminator
+	char *string_data; // Pointer to the string data, should be NULL terminated
+};
+
+int encode_negotiate_redfish_parameters_req(uint8_t instance_id,
+					    uint8_t mc_concurrency_support,
+					    bitfield16_t *mc_feature_support,
+					    struct pldm_msg *msg);
+
+int decode_negotiate_redfish_parameters_req(const struct pldm_msg *msg,
+					    uint8_t *mc_concurrency_support,
+					    bitfield16_t *mc_feature_support);
+
+int encode_negotiate_redfish_parameters_resp(
+	uint8_t instance_id, uint8_t completion_code,
+	uint8_t device_concurrency_support,
+	bitfield8_t *device_capabilities_flags,
+	bitfield16_t *device_feature_support,
+	uint32_t device_configuration_signature,
+	const char *device_provider_name,
+	enum pldm_rde_varstring_format name_format, size_t payload_length,
+	struct pldm_msg *msg);
+
+int decode_negotiate_redfish_parameters_resp(
+	const struct pldm_msg *msg, size_t payload_length,
+	uint8_t *completion_code, uint8_t *device_concurrency_support,
+	bitfield8_t *device_capabilities_flags,
+	bitfield16_t *device_feature_support,
+	uint32_t *device_configuration_signature,
+	struct pldm_rde_varstring *provider_name);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* LIBPLDM_RDE_H */

a4c9ed063bb6bab5b8ff6a7891fccb2fd7e20c9c
Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>
2025-10-22 05:11:40 -0500

    RDE: Add GetRegistryCount cmd support
    Add encode and decode APIs for GetRegistryCount command.
    
    Tests:
    Unit tests passed.
    
    Signed-off-by: Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>

--- src/dsp/rde.c
+++ src/dsp/rde.c
@@ -2082,3 +2082,89 @@ int decode_get_oem_count_resp(const struct pldm_msg *msg, size_t payload_length,
 
 	return pldm_msgbuf_complete(buf);
 }
+
+LIBPLDM_ABI_STABLE
+int encode_get_registry_count_req(uint8_t instance_id, struct pldm_msg *msg)
+{
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	struct pldm_header_info header = { 0 };
+	header.instance = instance_id;
+	header.pldm_type = PLDM_RDE;
+	header.msg_type = PLDM_REQUEST;
+	header.command = PLDM_GET_REGISTRY_COUNT;
+
+	return pack_pldm_header(&header, &(msg->hdr));
+}
+
+LIBPLDM_ABI_STABLE
+int encode_get_registry_count_resp(uint8_t instance_id, uint8_t completion_code,
+				   uint8_t registry_count,
+				   size_t payload_length, struct pldm_msg *msg)
+{
+	PLDM_MSGBUF_DEFINE_P(buf);
+	int rc;
+
+	if (NULL == msg) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	struct pldm_header_info header = { 0 };
+	header.msg_type = PLDM_RESPONSE;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_RDE;
+	header.command = PLDM_GET_REGISTRY_COUNT;
+
+	rc = pack_pldm_header(&header, &(msg->hdr));
+	if (rc != PLDM_SUCCESS) {
+		return rc;
+	}
+
+	rc = pldm_msgbuf_init_errno(buf, PLDM_GET_REGISTRY_COUNT_RES_BYTES,
+				    msg->payload, payload_length);
+	if (rc != PLDM_SUCCESS) {
+		fprintf(stderr, "init failed\n");
+		return rc;
+	}
+
+	pldm_msgbuf_insert(buf, completion_code);
+	if (completion_code != PLDM_SUCCESS) {
+		return PLDM_SUCCESS;
+	}
+
+	pldm_msgbuf_insert(buf, registry_count);
+
+	return pldm_msgbuf_complete(buf);
+}
+
+LIBPLDM_ABI_STABLE
+int decode_get_registry_count_resp(const struct pldm_msg *msg,
+				   size_t payload_length,
+				   uint8_t *completion_code,
+				   uint8_t *registry_count)
+{
+	PLDM_MSGBUF_DEFINE_P(buf);
+	int rc;
+
+	if (msg == NULL || completion_code == NULL || registry_count == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	rc = pldm_msgbuf_init_errno(buf, PLDM_GET_REGISTRY_COUNT_RES_BYTES,
+				    msg->payload, payload_length);
+	if (rc != PLDM_SUCCESS) {
+		fprintf(stderr, "init failed\n");
+		return rc;
+	}
+
+	pldm_msgbuf_extract_p(buf, completion_code);
+	if (*completion_code != PLDM_SUCCESS) {
+		return PLDM_SUCCESS;
+	}
+
+	pldm_msgbuf_extract_p(buf, registry_count);
+
+	return pldm_msgbuf_complete(buf);
+}

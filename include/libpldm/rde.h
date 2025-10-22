64f590c72a36b68e6437b66ed7fd73661f541a6d
Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>
2025-10-22 05:11:40 -0500

    RDE: Add GetRegistryCount cmd support
    Add encode and decode APIs for GetRegistryCount command.
    
    Tests:
    Unit tests passed.
    
    Signed-off-by: Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>

--- include/libpldm/rde.h
+++ include/libpldm/rde.h
@@ -37,6 +37,7 @@ extern "C" {
 #define PLDM_RDE_OPERATION_ENUMERATE_RESP_FIXED_BYTES	    3
 #define PLDM_GET_OEM_COUNT_REQ_BYTES			    5
 #define PLDM_GET_OEM_COUNT_RES_BYTES			    2
+#define PLDM_GET_REGISTRY_COUNT_RES_BYTES		    2
 
 enum pldm_rde_commands {
 	PLDM_NEGOTIATE_REDFISH_PARAMETERS = 0x01,
@@ -45,6 +46,7 @@ enum pldm_rde_commands {
 	PLDM_GET_SCHEMA_URI = 0x04,
 	PLDM_GET_RESOURCE_ETAG = 0x05,
 	PLDM_GET_OEM_COUNT = 0x06,
+	PLDM_GET_REGISTRY_COUNT = 0x08,
 	PLDM_RDE_OPERATION_INIT = 0x10,
 	PLDM_RDE_OPERATION_COMPLETE = 0x13,
 	PLDM_RDE_OPERATION_STATUS = 0x14,
@@ -1004,6 +1006,43 @@ int encode_get_oem_count_resp(uint8_t instance_id, uint8_t completion_code,
  */
 int decode_get_oem_count_resp(const struct pldm_msg *msg, size_t payload_length,
 			      uint8_t *completion_code, uint8_t *oem_count);
+
+/**
+ * @brief Encode GetRegistryCount request.
+ *
+ * @param[in] instance_id - Message's instance ID.
+ * @param[out] msg - Encoded PLDM request message.
+ * @return pldm_completion_codes.
+ */
+int encode_get_registry_count_req(uint8_t instance_id, struct pldm_msg *msg);
+
+/**
+ * @brief Encode GetRegistryCount response.
+ *
+ * @param[in] instance_id - Message's instance ID.
+ * @param[in] completion_code - PLDM completion code.
+ * @param[in] registry_count - Number of registry entries.
+ * @param[in] payload_length - Length of the response message payload.
+ * @param[out] msg - Encoded PLDM response message.
+ * @return pldm_completion_codes.
+ */
+int encode_get_registry_count_resp(uint8_t instance_id, uint8_t completion_code,
+				   uint8_t registry_count,
+				   size_t payload_length, struct pldm_msg *msg);
+
+/**
+ * @brief Decode GetRegistryCount response.
+ *
+ * @param[in] msg - PLDM response message to decode.
+ * @param[in] payload_length - Length of the response message payload.
+ * @param[out] completion_code - Decoded PLDM completion code.
+ * @param[out] registry_count - Decoded registry count.
+ * @return pldm_completion_codes.
+ */
+int decode_get_registry_count_resp(const struct pldm_msg *msg,
+				   size_t payload_length,
+				   uint8_t *completion_code,
+				   uint8_t *registry_count);
 
 #ifdef __cplusplus
 }
